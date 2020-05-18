596c4574a5792507488ec7e4643af089f028823b
Lawrene16 <thrillerbrown106@gmail.com>
2020-05-18 13:15:05 +0100

    Before reading csv file

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,241 @@
 import { Component } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import * as papa from "papaparse";
+declare var ProgressBar: any;
+
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
 })
 export class HomePage {
+  cards;
+  imageData = [];
+  subjectsListStrings = [];
+  fullSubjectList = [];
+  bar: any;
+  i = 0;
 
-  constructor() {}
 
+  constructor(public httpClient: HttpClient) {
+    this.cards = [];
+    setTimeout(() => {
+      this.loadTinderCards();
+      // console.log(this.cards)
+      // this.readCsvData();
+
+      // this.move();
+    }, 300);
+  }
+
+  // setUpProgressBar() {
+  //   this.bar = new ProgressBar.Line("#progress", {
+  //     strokeWidth: 4,
+  //     easing: "easeInOut",
+  //     duration: 800,
+  //     color: "#4CAF50",
+  //     trailColor: "#eee",
+  //     // radius: '25px',
+  //     trailWidth: 5,
+  //     svgStyle: { width: "100%", height: "100%" },
+  //   });
+
+  //   setTimeout(() => {
+  //     this.bar.animate(0.7); // Number from 0.0 to 1.0
+  //   }, 800);
+  // }
+
+  loadTinderCards() {
+    this.cards = [
+      {
+        img: "https://placeimg.com/300/300/people",
+        title: "Demo card 1",
+        description: "This is a demo for Tinder like swipe cards",
+      },
+      {
+        img: "https://placeimg.com/300/300/animals",
+        title: "Demo card 2",
+        description: "This is a demo for Tinder like swipe cards",
+      },
+      {
+        img: "https://placeimg.com/300/300/nature",
+        title: "Demo card 3",
+        description: "This is a demo for Tinder like swipe cards",
+      },
+      {
+        img: "https://placeimg.com/300/300/tech",
+        title: "Demo card 4",
+        description: "This is a demo for Tinder like swipe cards",
+      },
+      {
+        img: "https://placeimg.com/300/300/arch",
+        title: "Demo card 5",
+        description: "This is a demo for Tinder like swipe cards",
+      },
+      {
+        img: "https://placeimg.com/300/300/people",
+        title: "Demo card 1",
+        description: "This is a demo for Tinder like swipe cards",
+      },
+      {
+        img: "https://placeimg.com/300/300/animals",
+        title: "Demo card 2",
+        description: "This is a demo for Tinder like swipe cards",
+      },
+      {
+        img: "https://placeimg.com/300/300/nature",
+        title: "Demo card 3",
+        description: "This is a demo for Tinder like swipe cards",
+      },
+      {
+        img: "https://placeimg.com/300/300/tech",
+        title: "Demo card 4",
+        description: "This is a demo for Tinder like swipe cards",
+      },
+      {
+        img: "https://placeimg.com/300/300/arch",
+        title: "Demo card 5",
+        description: "This is a demo for Tinder like swipe cards",
+      },
+      {
+        img: "https://placeimg.com/300/300/people",
+        title: "Demo card 1",
+        description: "This is a demo for Tinder like swipe cards",
+      },
+      {
+        img: "https://placeimg.com/300/300/animals",
+        title: "Demo card 2",
+        description: "This is a demo for Tinder like swipe cards",
+      },
+      {
+        img: "https://placeimg.com/300/300/nature",
+        title: "Demo card 3",
+        description: "This is a demo for Tinder like swipe cards",
+      },
+      {
+        img: "https://placeimg.com/300/300/tech",
+        title: "Demo card 4",
+        description: "This is a demo for Tinder like swipe cards",
+      },
+      {
+        img: "https://placeimg.com/300/300/arch",
+        title: "Demo card 5",
+        description: "This is a demo for Tinder like swipe cards",
+      },
+      {
+        img: "https://placeimg.com/300/300/people",
+        title: "Demo card 1",
+        description: "This is a demo for Tinder like swipe cards",
+      },
+      {
+        img: "https://placeimg.com/300/300/animals",
+        title: "Demo card 2",
+        description: "This is a demo for Tinder like swipe cards",
+      },
+      {
+        img: "https://placeimg.com/300/300/nature",
+        title: "Demo card 3",
+        description: "This is a demo for Tinder like swipe cards",
+      },
+      {
+        img: "https://placeimg.com/300/300/tech",
+        title: "Demo card 4",
+        description: "This is a demo for Tinder like swipe cards",
+      },
+      {
+        img: "https://placeimg.com/300/300/arch",
+        title: "Demo card 5",
+        description: "This is a demo for Tinder like swipe cards",
+      },
+    ];
+  }
+
+//   readCsvData() {
+//     this.httpClient.get("assets/final.json").subscribe(
+//       (data) => this.extractData(data),
+//       (err) => this.handleError(err)
+//     );
+//   }
+
+//   private handleError(err) {
+//     console.log("something went wrong: ", err);
+//   }
+
+//   trackByFn(index: any, item: any) {
+//     return index;
+//   }
+
+// move() {
+//   if (this.i == 0) {
+//     this.i = 1;
+//     var elem = document.getElementById("progress");
+//     var width = 1;
+//     var id = setInterval(frame, 10);
+//     function frame() {
+//       if (width >= 100) {
+//         clearInterval(id);
+//         this.i = 0;
+//       } else {
+//         width++;
+//         elem.style.width = width + "%";
+//       }
+//     }
+//   }
+// }
+
+//   private extractData(res) {
+//     res.forEach((element) => {
+//       if (!this.subjectsListStrings.includes(element.Subject)) {
+//         this.subjectsListStrings.push(element.Subject);
+//         console.log("includes");
+//       } else {
+//         console.log("doesnt include");
+//       }
+//     });
+
+//     // console.log(this.subjectsListStrings);
+
+//     for (var i = 0; i < this.subjectsListStrings.length; i++) {
+//       this.fullSubjectList.push({
+//         title: this.subjectsListStrings[i],
+//         img: this.imageData[i].img,
+//         lastReview: "Last Review April 09, 2020",
+//         progress: 78
+//       });
+//       // this.subjectsListStrings[i].img = this.imageData[i].img;
+//     }
+
+//     console.log(this.fullSubjectList);
+
+//     // // console.log(this.subjectsList)
+//     // for (var i = 0; i < 10; i++) {
+//     //   this.cards.push(res[i]);
+//     //   this.cards[i].img = this.imageData[i].img;
+//     //   // console.log(res[i]);
+//     // }
+//     // this.cards = res;
+//     // console.log(res)
+//     // res.forEach(row => {
+//     //   console.log(row)
+//     // });
+
+//     // let csvData = res["_body"] || "";
+//     // let parsedData = papa.parse(csvData).data;
+
+//     // console.log(parsedData)
+
+//     // this.headerRow = parsedData[0];
+
+//     // parsedData.splice(0, 1);
+//     // this.csvData = parsedData;
+//   }
+
+  logChoice(event) {
+    // if (this.choiceMadeEventCount == 0) {
+    //   this.choiceMadeEventCount++;
+    //   // console.log(event);
+    // }
+    // this.choiceMadeEventCount = 0;
+  }
 }
---
+++ src/app/tinder-ui/tinder-ui.component.ts
@@ -0,0 +1,215 @@
+import {
+  Component,
+  Input,
+  ViewChildren,
+  QueryList,
+  ElementRef,
+  Renderer2,
+  Output,
+  EventEmitter,
+} from "@angular/core";
+
+declare var ProgressBar: any;
+
+@Component({
+  selector: "app-tinder-ui",
+  templateUrl: "./tinder-ui.component.html",
+  styleUrls: ["./tinder-ui.component.scss"],
+})
+export class TinderUiComponent {
+  @Output() choiceMade = new EventEmitter();
+  @Input("cards") cards: Array<{
+    img: string;
+    title: string;
+    description: string;
+  }>;
+  cardOpacity
+  bar: any;
+  @ViewChildren("tinderCard") tinderCards: QueryList<ElementRef>;
+  tinderCardsArray: Array<ElementRef>;
+
+  moveOutWidth: number; // value in pixels that a card needs to travel to dissapear from screen
+  shiftRequired: boolean; // state variable that indicates we need to remove the top card of the stack
+  transitionInProgress: boolean; // state variable that indicates currently there is transition on-going
+  heartVisible: boolean;
+  crossVisible: boolean;
+
+  constructor(private renderer: Renderer2) {
+    setTimeout(() => {
+      let tinderCardElement = document.getElementById("tindercards");
+      let hamming = new Hammer(tinderCardElement);
+      hamming.on("panleft panright tap press pressup panend", (ev) => {
+        if (ev.type == "panleft") {
+          this.userClickedButton(ev, false);
+        } else if (ev.type == "panright") {
+          this.userClickedButton(ev, true);
+        } else if (ev.type == "panend") {
+        }
+      });
+
+      this.setUpProgressBar();
+    }, 300);
+
+
+  }
+
+  setUpProgressBar(){
+      this.bar = new ProgressBar.Line("#progress", {
+        strokeWidth: 4,
+        easing: "easeInOut",
+        duration: 800,
+        color: "#4CAF50",
+        progress: 0.6,
+        trailColor: "#eee",
+        // radius: '25px',
+        trailWidth: 5,
+        svgStyle: { width: "100%", height: "100%" },
+      });
+
+    setTimeout(() => {
+      this.bar.animate(0.7); // Number from 0.0 to 1.0
+    }, 800);
+  }
+
+  userClickedButton(event, heart) {
+    event.preventDefault();
+    if (!this.cards.length) return false;
+    if (heart) {
+      this.renderer.setStyle(
+        this.tinderCardsArray[0].nativeElement,
+        "transform",
+        "translate(" + this.moveOutWidth + "px, -100px) rotate(-30deg)"
+      );
+      this.toggleChoiceIndicator(false, true);
+      this.emitChoice(heart, this.cards[0]);
+    } else {
+      this.renderer.setStyle(
+        this.tinderCardsArray[0].nativeElement,
+        "transform",
+        "translate(-" + this.moveOutWidth + "px, -100px) rotate(30deg)"
+      );
+      this.toggleChoiceIndicator(true, false);
+      this.emitChoice(heart, this.cards[0]);
+    }
+    this.shiftRequired = true;
+    this.transitionInProgress = true;
+  }
+
+  handlePan(event) {
+    // console.log("event")
+    if (
+      event.deltaX === 0 ||
+      (event.center.x === 0 && event.center.y === 0) ||
+      !this.cards.length
+    )
+      return;
+
+    if (this.transitionInProgress) {
+      this.handleShift();
+    }
+
+    this.renderer.addClass(this.tinderCardsArray[0].nativeElement, "moving");
+
+    if (event.deltaX > 0) {
+      this.toggleChoiceIndicator(false, true);
+    }
+    if (event.deltaX < 0) {
+      this.toggleChoiceIndicator(true, false);
+    }
+
+    let xMulti = event.deltaX * 0.03;
+    let yMulti = event.deltaY / 80;
+    let rotate = xMulti * yMulti;
+
+    this.renderer.setStyle(
+      this.tinderCardsArray[0].nativeElement,
+      "transform",
+      "translate(" +
+        event.deltaX +
+        "px, " +
+        event.deltaY +
+        "px) rotate(" +
+        rotate +
+        "deg)"
+    );
+
+    this.shiftRequired = true;
+  }
+
+  handlePanEnd(event) {
+    this.toggleChoiceIndicator(false, false);
+
+    if (!this.cards.length) return;
+
+    this.renderer.removeClass(this.tinderCardsArray[0].nativeElement, "moving");
+
+    let keep = Math.abs(event.deltaX) < 80 || Math.abs(event.velocityX) < 0.5;
+    if (keep) {
+      this.renderer.setStyle(
+        this.tinderCardsArray[0].nativeElement,
+        "transform",
+        ""
+      );
+      this.shiftRequired = false;
+    } else {
+      let endX = Math.max(
+        Math.abs(event.velocityX) * this.moveOutWidth,
+        this.moveOutWidth
+      );
+      let toX = event.deltaX > 0 ? endX : -endX;
+      let endY = Math.abs(event.velocityY) * this.moveOutWidth;
+      let toY = event.deltaY > 0 ? endY : -endY;
+      let xMulti = event.deltaX * 0.03;
+      let yMulti = event.deltaY / 80;
+      let rotate = xMulti * yMulti;
+
+      this.renderer.setStyle(
+        this.tinderCardsArray[0].nativeElement,
+        "transform",
+        "translate(" +
+          toX +
+          "px, " +
+          (toY + event.deltaY) +
+          "px) rotate(" +
+          rotate +
+          "deg)"
+      );
+
+      this.shiftRequired = true;
+
+      this.emitChoice(!!(event.deltaX > 0), this.cards[0]);
+    }
+    this.transitionInProgress = true;
+  }
+
+  toggleChoiceIndicator(cross, heart) {
+    this.crossVisible = cross;
+    this.heartVisible = heart;
+  }
+
+  handleShift() {
+    this.transitionInProgress = false;
+    this.toggleChoiceIndicator(false, false);
+    if (this.shiftRequired) {
+      this.shiftRequired = false;
+      this.cards.shift();
+    }
+
+    console.log("shift handlee")
+  }
+
+  emitChoice(heart, card) {
+    this.choiceMade.emit({
+      choice: heart,
+      payload: card,
+    });
+  }
+
+  ngAfterViewInit() {
+    this.moveOutWidth = document.documentElement.clientWidth * 1.5;
+    this.tinderCardsArray = this.tinderCards.toArray();
+    this.tinderCards.changes.subscribe(() => {
+      this.tinderCardsArray = this.tinderCards.toArray();
+    });
+  }
+}
